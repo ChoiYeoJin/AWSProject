fb123c49f5208a98f8bdd90872b20137df162ac4
ChoiYeoJin <sonata7531@gmail.com>
2019-04-29 17:14:36 +0900

    lambda code

---
+++ AWSLambda/AWSManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon.S3.Transfer;
+using Amazon.S3;
+using System.IO;
+
+namespace AWSLambda
+{
+    class AWSManager
+    {
+        public static void UploadFile(MemoryStream parquetFile, string bucketName, string keyName)
+        {
+            using (var fileTransferUtility = new TransferUtility(new AmazonS3Client()))
+            {
+                fileTransferUtility.Upload(parquetFile, bucketName, keyName + ".parquet");
+            }
+            parquetFile.Close();
+        }
+
+    }
+}
---
+++ AWSLambda/FileManager.cs
@@ -0,0 +1,161 @@
+﻿using Parquet;
+using Parquet.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AWSLambda
+{
+    class FileManager
+    {
+        public static char delimiter = ',';
+        public static int lineLimit = 100000;
+        public static void EventToParquet(StreamReader reader, string key)
+        {
+            var schema = new Schema(
+                                new DataField<string>("identity_adid"),
+                                new DataField<string>("os"),
+                                new DataField<string>("model"),
+                                new DataField<string>("country"),
+                                new DataField<string>("event_name"),
+                                new DataField<string>("log_id"),
+                                new DataField<string>("server_datetime"),
+                                new DataField<int?>("quantity"),
+                                new DataField<float?>("price"));
+            var parquetTable = new Parquet.Data.Rows.Table(schema);
+
+            string line = "";
+            int i;
+            float d;
+            int lineCount = 1;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (lineCount % lineLimit == 0)
+                {
+                    AWSManager.UploadFile(WriteParquet(schema, parquetTable),
+                        "converted-data/event", key + (lineCount / lineLimit).ToString());
+                    parquetTable.Clear();
+                    parquetTable = new Parquet.Data.Rows.Table(schema);
+                }
+                if (!string.IsNullOrEmpty(line))
+                {
+                    line = line.Replace("\"", "");
+                    var splited = line.Split(delimiter);
+                    parquetTable.Add(new Parquet.Data.Rows.Row(splited[0], splited[1], splited[2], splited[3], splited[4], splited[5],
+                        splited[6], int.TryParse(splited[7], out i) ? (int?)i : null,
+                        float.TryParse(splited[8], out d) ? (float?)d : null));
+                }
+                lineCount++;
+            }
+            if (parquetTable.Count != 0)
+                AWSManager.UploadFile(WriteParquet(schema, parquetTable),
+                        "converted-data/event", key + (lineCount / lineLimit+1).ToString());
+            reader.Close();
+
+            /*var schema = new Schema(
+                                new DataField<string>("identity_adid"),
+                                new DataField<string>("os"),
+                                new DataField<string>("model"),
+                                new DataField<string>("country"),
+                                new DataField<string>("event_name"),
+                                new DataField<string>("log_id"),
+                                new DataField<string>("server_datetime"),
+                                new DataField<int?>("quantity"),
+                                new DataField<decimal?>("price"));
+            var parquetTable = new Parquet.Data.Rows.Table(schema);
+
+            string line = "";
+            int i;
+            decimal d;
+            int lineCount = 1;
+            string currentData = "";
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    line = line.Replace("\"", "");
+                    var splited = line.Split(delimiter);
+                    string date = splited[6].Substring(0, 10);
+                    if (date != currentData)
+                    {
+                        if (currentData != "")
+                        {
+                            AWSManager.UploadFile(WriteParquet(schema, parquetTable),
+                                "converted-data/event-date/dt=" + currentData, key + currentData + DateTime.Now.ToString("yyyyMMddTHHmmss"));
+                            parquetTable.Clear();
+                            parquetTable = new Parquet.Data.Rows.Table(schema);
+                            Console.WriteLine(lineCount.ToString());
+                        }
+                        
+                        currentData = date;
+                    }
+                    parquetTable.Add(new Parquet.Data.Rows.Row(splited[0], splited[1], splited[2], splited[3], splited[4], splited[5],
+                        splited[6], int.TryParse(splited[7], out i) ? (int?)i : null,
+                        decimal.TryParse(splited[8], out d) ? (decimal?)d : null));
+                }
+                lineCount++;
+            }
+            if (parquetTable.Count != 0)
+                AWSManager.UploadFile(WriteParquet(schema, parquetTable),
+                        "converted-data/event", key + (lineCount / lineLimit + 1).ToString());
+            reader.Close();*/
+        }
+
+        public static void AttrToParquet(StreamReader reader, string key)
+        {
+            var schema = new Schema(
+                    new DataField<string>("partner"),
+                    new DataField<string>("campaign"),
+                    new DataField<string>("server_datetime"),
+                    new DataField<string>("tracker_id"),
+                    new DataField<string>("log_id"),
+                    new DataField<int?>("attribution_type"),
+                    new DataField<string>("identity_adid"));
+            var parquetTable = new Parquet.Data.Rows.Table(schema);
+
+            string line = "";
+            int i;
+            int lineCount = 1;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if(lineCount%lineLimit == 0)
+                {
+                    AWSManager.UploadFile(WriteParquet(schema, parquetTable),
+                        "converted-data/attr", key + (lineCount / lineLimit).ToString());
+                    parquetTable.Clear();
+                    parquetTable = new Parquet.Data.Rows.Table(schema);
+                }
+                if (!string.IsNullOrEmpty(line))
+                {
+                    line = line.Replace("\"", "");
+                    var splited = line.Split(delimiter);
+                    parquetTable.Add(new Parquet.Data.Rows.Row(splited[0], splited[1], splited[2],
+                        splited[3], splited[4], int.TryParse(splited[5], out i) ? (int?)i : null, splited[6]));
+                }
+                lineCount++;
+            }
+            
+            if(parquetTable.Count != 0)
+                AWSManager.UploadFile(WriteParquet(schema, parquetTable),
+                    "converted-data/attr", "part-00000-" + (lineCount / lineLimit+1).ToString());
+            reader.Close();
+        }
+
+        public static MemoryStream WriteParquet(Schema schema, Parquet.Data.Rows.Table parquetTable)
+        {
+            MemoryStream fileStream = new MemoryStream();
+                using (var parquetWriter = new Parquet.ParquetWriter(schema, fileStream))
+                {
+                    using (ParquetRowGroupWriter groupWriter = parquetWriter.CreateRowGroup())
+                    {
+                        groupWriter.Write(parquetTable);
+                    }
+
+                }
+            return fileStream;
+            
+        }
+
+    }
+}
